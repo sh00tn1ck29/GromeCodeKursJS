828f6ba000b7ab2bea090c81afea5b17d3e864c2
sh00tn1ck29 <maksimsavrygin21@gmail.com>
2026-04-23 19:13:27 +0200

    Update getMinSquaredNumbers.js

--- lesson13/task3/getMinSquaredNumbers.js
+++ lesson13/task3/getMinSquaredNumbers.js
@@ -1,1 +1,12 @@
+export default (arr) => {
+  if (!Array.isArray(arr) || arr.length === 0) {
+    return null;
+  }
+  const arrAbs = arr.map((element) => {
+    return Math.abs(element);
+  });
+
+  const minNumber = Math.min(...arrAbs);
+  return minNumber * minNumber;
+};
 
