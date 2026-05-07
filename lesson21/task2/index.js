66458350184715350e2dfe9dd679def13889a891
sh00tn1ck29 <maksimsavrygin21@gmail.com>
2026-05-07 16:04:14 +0200

    Update index.js

--- lesson21/task2/index.js
+++ lesson21/task2/index.js
@@ -1,11 +1,11 @@
 export function getTitleElement() {
-  titleElement = document.querySelector('.title');
-  console.log(titleElement);
-  return titleElement;
+  const titleElem = document.querySelector('.title');
+  console.dir(titleElem);
+  return titleElem;
 }
 
 export function getInputElement() {
-  inputElement = document.querySelector('input').type = 'text';
-  console.log(inputElement);
-  return inputElement;
+  const inputElem = document.querySelector('input[type="text"]');
+  console.dir(inputElem);
+  return inputElem;
 }
