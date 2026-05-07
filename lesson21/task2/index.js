ccb8a64b6ef90eae471691eb1ddbea64e11ae018
sh00tn1ck29 <maksimsavrygin21@gmail.com>
2026-05-07 15:57:58 +0200

    Add functions to get title and input elements

---
+++ lesson21/task2/index.js
@@ -0,0 +1,11 @@
+export function getTitleElement() {
+  titleElement = document.querySelector('.title');
+  console.log(titleElement);
+  return titleElement;
+}
+
+export function getInputElement() {
+  inputElement = document.querySelector('input').type = 'text';
+  console.log(inputElement);
+  return inputElement;
+}
