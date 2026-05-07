081de268cccba6192934585d37e47964ecf725cd
sh00tn1ck29 <maksimsavrygin21@gmail.com>
2026-05-07 17:55:50 +0200

    Create index.js

---
+++ lesson21/task13/index.js
@@ -0,0 +1,9 @@
+export function getSection(number) {
+  export function getSection(num) {
+    const spanEl = document.querySelector(`span[data-number="${num}"]`);
+
+    const parentBox = spanEl.closest('.box');
+
+    return parentBox.dataset.section;
+  }
+}
