8c56824417573b98538bce81d1031e96f7fad2e9
sh00tn1ck29 <maksimsavrygin21@gmail.com>
2026-05-20 19:24:05 +0200

    Update todoList.js

--- lessonLocaLStorage/todoList.js
+++ lessonLocaLStorage/todoList.js
@@ -1,10 +1,10 @@
-import { createTaskHandler } from './createTask.js';
-import { toggleTaskHandler } from './updateTask.js';
+import { onCreateTask } from './createTask.js';
+import { onToggleTask } from './updateTask.js';
 
 export const initTodoListHandlers = () => {
   const createButton = document.querySelector('.create-task-btn');
   const listElem = document.querySelector('.list');
 
-  createButton.addEventListener('click', createTaskHandler);
-  listElem.addEventListener('click', toggleTaskHandler);
+  createButton.addEventListener('click', onCreateTask);
+  listElem.addEventListener('click', onToggleTask);
 };
