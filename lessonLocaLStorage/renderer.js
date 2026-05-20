579144e654fa731a5f0b09d1e56502c126a9c883
sh00tn1ck29 <maksimsavrygin21@gmail.com>
2026-05-20 19:37:38 +0200

    Update renderer.js

--- lessonLocaLStorage/renderer.js
+++ lessonLocaLStorage/renderer.js
@@ -2,47 +2,31 @@ import { getItem } from './storage.js';
 
 const listElem = document.querySelector('.list');
 
-const compareTasks = (a, b) => {
-  if (a.done !== b.done) {
-    return a.done - b.done;
-  }
-  if (!a.done) {
-    return new Date(b.createDate) - new Date(a.createDate);
-  }
-  return new Date(b.doneDate) - new Date(a.doneDate);
-};
-
-const createCheckbox = ({ done, id }) => {
-  const checkboxElem = document.createElement('input');
-  checkboxElem.setAttribute('type', 'checkbox');
-  checkboxElem.dataset.id = id;
-  checkboxElem.checked = done;
-  checkboxElem.classList.add('list__item-checkbox');
-  return checkboxElem;
-};
-
-const createListItem = ({ text, done, id }) => {
-  const listItemElem = document.createElement('li');
-  listItemElem.classList.add('list__item');
-  
-  const checkboxElem = createCheckbox({ done, id });
-  
-  if (done) {
-    listItemElem.classList.add('list__item_done');
-  }
-  
-  listItemElem.append(checkboxElem, text);
-  return listItemElem;
-};
-
 export const renderTasks = () => {
   const tasksList = getItem('tasksList') || [];
   
   listElem.innerHTML = '';
-  
+
   const tasksElems = tasksList
-    .sort(compareTasks)
-    .map(createListItem);
-    
+    .slice()
+    .sort((a, b) => a.done - b.done)
+    .map(({ id, text, done }) => {
+      const listItemElem = document.createElement('li');
+      listItemElem.classList.add('list__item');
+
+      const checkbox = document.createElement('input');
+      checkbox.setAttribute('type', 'checkbox');
+      checkbox.checked = done;
+      checkbox.dataset.id = id;
+      checkbox.classList.add('list__item-checkbox');
+
+      if (done) {
+        listItemElem.classList.add('list__item_done');
+      }
+
+      listItemElem.append(checkbox, text);
+      return listItemElem;
+    });
+
   listElem.append(...tasksElems);
 };
