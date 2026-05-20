0babfee889e7ab9c9ac037cdbb4ff8f48d050154
sh00tn1ck29 <maksimsavrygin21@gmail.com>
2026-05-20 17:53:24 +0200

    Create index.js

---
+++ lesson27/task2/index.js
@@ -0,0 +1,25 @@
+const counterElement = document.querySelector('.counter');
+const counterValueElem = document.querySelector('.counter__value');
+const counterChangeClick = (e) => {
+  const isButton = e.target.classList.contains('counter__button');
+
+  if (!isButton) {
+    return;
+  }
+
+  const action = e.target.dataset.action;
+  const oldValue = Number(counterValueElem.textContent);
+
+  const newValue = action === 'decrease' ? oldValue - 1 : oldValue + 1;
+
+  localStorage.setItem('counterValue', newValue);
+  counterValueElem.textContent = newValue;
+};
+
+counterElement.addEventListener('click', counterChangeClick);
+
+const onStorageChange = (e) => {
+  counterValueElem.textContent = e.newValue;
+};
+
+window.addEventListener('storage', onStorageChange);
