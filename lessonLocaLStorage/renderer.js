405045b2c46b8cafed9a930e826ce3838abdc91e
sh00tn1ck29 <maksimsavrygin21@gmail.com>
2026-05-20 18:58:37 +0200

    Create renderer.js

---
+++ lessonLocaLStorage/renderer.js
@@ -0,0 +1,35 @@
+const listElem = document.querySelector('.list');
+
+export const renderTasks = (tasksList) => {
+  listElem.innerHTML = '';
+
+  const tasksElems = [...tasksList]
+    .sort((a, b) => {
+      if (a.done !== b.done) {
+        return a.done - b.done;
+      }
+      if (!a.done) {
+        return new Date(b.createDate) - new Date(a.createDate);
+      }
+      return new Date(b.doneDate) - new Date(a.doneDate);
+    })
+    .map(({ text, done, id }) => {
+      const listItemElem = document.createElement('li');
+      listItemElem.classList.add('list__item');
+
+      const checkbox = document.createElement('input');
+      checkbox.setAttribute('type', 'checkbox');
+      checkbox.dataset.id = id;
+      checkbox.checked = done;
+      checkbox.classList.add('list__item-checkbox');
+
+      if (done) {
+        listItemElem.classList.add('list__item_done');
+      }
+
+      listItemElem.append(checkbox, text);
+      return listItemElem;
+    });
+
+  listElem.append(...tasksElems);
+};
