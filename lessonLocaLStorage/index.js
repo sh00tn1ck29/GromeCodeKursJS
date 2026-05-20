c094e1eceeeaa131fab76767879702ef5ae8cde4
sh00tn1ck29 <maksimsavrygin21@gmail.com>
2026-05-20 19:13:59 +0200

    Update index.js

--- lessonLocaLStorage/index.js
+++ lessonLocaLStorage/index.js
@@ -2,9 +2,22 @@ import { initTodoListHandlers } from './todoList.js';
 import { renderTasks } from './renderer.js';
 import { getItem } from './storage.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const tasks = getItem('tasksList') || [];
+const onStorageChange = (e) => {
+  if (e.key === 'tasksList') {
+    
+    const updatedTasks = e.newValue ? JSON.parse(e.newValue) : [];
+    renderTasks(updatedTasks);
+  }
+};
 
+document.addEventListener('DOMContentLoaded', () => {
+  
+  const tasks = getItem('tasksList') || [];
   renderTasks(tasks);
+  
+  
   initTodoListHandlers();
 });
+
+
+window.addEventListener('storage', onStorageChange);
