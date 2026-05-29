1bbd1859a1c3d14210b6c254db9dfcab667da038
sh00tn1ck29 <maksimsavrygin21@gmail.com>
2026-05-29 22:11:38 +0200

    Create spinner.js

---
+++ lesson36/task1/spinner.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const spinnerElem = document.querySelector('.spinner');
+
+export const showSpinner = () => {
+  spinnerElem.classList.remove('spinner_hidden');
+};
+
+export const hideSpinner = () => {
+  spinnerElem.classList.add('spinner_hidden');
+};
